7c89f03cf9c41c4f7b5119a3b70b191709c41b6a
domenhribernik <domen.hribernik4@gmail.com>
2025-05-05 10:42:33 +0300

    Added chat

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', event => {
     window.addEventListener('load', () => {
         const loadingOverlay = document.getElementById('loading-overlay');
         loadingOverlay.classList.add('fade-out');
-    
+
         setTimeout(() => {
             loadingOverlay.remove();
         }, 2000);
@@ -139,3 +139,13 @@ window.document.addEventListener('DOMContentLoaded', async () => {
         console.error("Error fetching or displaying the quote:", error);
     }
 });
+
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/681860d15d55ef191a9daf60/1iqfjkd3r';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
