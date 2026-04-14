8bc1a285ecdc04ea455778b53d70b58718749c27
domenhribernik <domen.hribernik4@gmail.com>
2026-04-14 14:03:05 +0200

    Added google analytics

---
+++ components/google-analytics.js
@@ -0,0 +1,9 @@
+var s = document.createElement("script");
+s.async = true;
+s.src = "https://www.googletagmanager.com/gtag/js?id=G-YKL3ML3L5J";
+document.head.appendChild(s);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-YKL3ML3L5J');
