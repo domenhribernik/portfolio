ecee2aef200097c2d929166c40b8bfeea8a48711
domenhribernik <domen.hribernik4@gmail.com>
2026-05-26 09:38:58 +0200

    Changed tawk widget property

--- components/tawk-chat.js
+++ components/tawk-chat.js
@@ -1,8 +1,9 @@
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/681860d15d55ef191a9daf60/1iqfjkd3r';
-    s1.charset = 'UTF-8';
-    s0.parentNode.insertBefore(s1, s0);
+(function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6a154d3fbc77081c353c6579/1jphjbgp4';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
 })();
